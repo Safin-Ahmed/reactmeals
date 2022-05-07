c77a33d4b67ec4ba3ceaa8117c9f4cb8fccc0f9d
Safin Ahmed <safin.ahmed2000@gmail.com>
2022-05-08 02:02:24 +0600

    Design is completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./component/Layout/Header";
+import Meals from "./component/Meals/Meals";
 
 function App() {
   return (
     <>
       <Header />
+      <Meals />
     </>
   );
 }
---
+++ src/component/Meals/AvailableMeals.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import Card from "../UI/Card";
+import classes from "./AvailableMeals.module.css";
+import MealItem from "./MealItem/MealItem";
+
+const DUMMY_MEALS = [
+  {
+    id: "m1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
+  },
+
+  {
+    id: "m2",
+    name: "Noodles",
+    description: "Maggie Instant Noodles",
+    price: 16.99,
+  },
+
+  {
+    id: "m3",
+    name: "Chillox Burger",
+    description: "A Dhanmondi Speciality",
+    price: 10.99,
+  },
+
+  {
+    id: "m4",
+    name: "Dominos Pizza",
+    description: "Boroloxx er Pizza",
+    price: 32.99,
+  },
+
+  {
+    id: "m5",
+    name: "Gorur Goshto",
+    description: "700 Taka Kg",
+    price: 122.99,
+  },
+];
+
+const AvailableMeals = () => {
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
